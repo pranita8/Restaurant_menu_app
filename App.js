2107fa55bfde595671c131a89780630782dcf978
pranita8 <91770829+pranita8@users.noreply.github.com>
2022-11-13 22:54:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Resturant from "./component/basics/Resturant";
+//import UseState from './component/hooks/useState';
+//import UseEffect from './component/hooks/useEffect.js';
+const App = () => {
+  //return <UseState />
+  //return<UseEffect/>
+return<Resturant/>
+ 
+};
+
+export default App;
---
+++ menuApi.js
@@ -0,0 +1,147 @@
+import { within } from "@testing-library/react";
+
+const Menu=[
+  {
+    id: 1,
+    image: "images/tomato_amlet.jpg",
+    name: "Tomato amlet",
+    category: "starters",
+    price: "30₹",
+    description:
+     " This is the special amlet that we have. ate consectetur quaerat ipsum rerum eum accusantium, sed magnam"
+  }, 
+
+  {
+    id: 2,
+    image: "images/Masala-papad.jpg",
+    name: "Masala Papad",
+    category: "starters",
+    price: "10₹",
+    description:
+     " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus iste voluptatum temporibus pariatur voluptatibus nostrum placeat sapiente, sed nihil. Nobis non cum, voluptatibus deleniti veritatis unde molestias corporis tenetur eligendi"
+  },
+  {
+    id: 3,
+    image: "images/Mexican-chicken-soup.jpg",
+    name: "Mexican Chicken Soup",
+    category: "starters",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, officia. Nostrum nam eveniet reprehenderit. Voluptatum libero unde, placeat ad quis incidunt debitis quibusdam quaerat eaque error ut, reiciendis numquam quas"
+  },
+  {
+    id: 4,
+    image: "images/crispy-corn.jpg",
+    name: "Crispy Corn",
+    category: "starters",
+    price: "10₹",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam voluptates libero quisquam cum dicta! Minima, quibusdam dolorum perferendis quia tenetur facere et ipsa voluptatem, ab accusantium veniam aut corporis quasi." 
+  },
+{
+    id: 3,
+    image: "images/Allo-Paratha.jpg",
+    name: "Allo Pratha",
+    category: "breakfast",
+    price: "50₹",
+    description:
+      "Allo paratha is a popular Indian breakfast of unleavened whole wheat savory and spiced potato stuffed flatbread from Punjab, India. This delicious Punjabi aloo paratha is one of the most loved breakfast at my place."
+      
+  },
+  {
+    id: 2,
+    image: "images/dosa.jpg",
+    name: "Dosa",
+    category: "breakfast",
+    price: "50₹",
+    description:
+    "A dosa is a thin pancake or crepe originating from South India, made from a fermented batter predominantly consisting of lentils and rice. ... Dosas are served hot along with chutney by tradition and sambar in recent times. "
+  },
+  {
+    id: 3,
+    image: "images/idli.jpg",
+    name: "Idli-Sambar",
+    category: "breakfast",
+    price: "40₹",
+    description:
+      "Idli Sambar is a hearty, satisfying, comforting and a healthy meal of soft fluffy idlis served with savory, spiced and lightly tangy sambar – a vegetable stew made with lentils and assorted vegetables. It is a winning combination made for each other."
+  },
+  {
+    id: 4,
+    image: "images/sandwich.jpg ",
+    name: "Sandwich",
+    category: "breakfast",
+    price: "50₹",
+    description:
+      "A sandwich is a food typically consisting of vegetables, sliced cheese or meat, placed on or between slices of bread, or more generally any dish wherein bread serves as a container or wrapper for another food type."
+  },
+  {
+    id: 5,
+    image: "images/omlete.jpg",
+    name: "Egg Omelet",
+    category: "breakfast",
+    price: "40₹",
+    description:
+      "omelet is a dish made from beaten eggs, fried with butter or oil in a frying pan . It is quite common for the omelette to be folded around fillings such as chives, vegetables, mushrooms, meat , cheese, or some combination of the above."
+  },
+  {
+    id: 6,
+    image: "images/cold coffe.jpg",
+    name: "Cold Coffe ",
+    category: "Beverage",
+    price: "50₹",
+    description:
+      "Instructions In a blender take your instant coffee, raw sugar, add warm water. Blend for a minute or until the coffee solution becomes frothy and the brown color lightens a bit."
+  },
+
+  {
+    id: 7,
+    image: "images/hot coffe.jpg",
+    name: "Coffe ",
+    category: "Beverage",
+    price: "50₹",
+    description:
+      "Probably the closest you will get to flavoured coffee in Tobys, the Cafe Mocha is made up of 50% Ghana cocoa and 50% Ivory Coast chocolate. It compliments the house blend really well, uplifts a lot of berriness from within, very round and fruity for a mocha. Delicious."
+
+       },
+     {
+       id: 8,
+       image: "images/green tea.jpg",
+       name: "Green Tea ",
+       category: "Beverage",
+       price: "30₹",
+       description:
+         "One cup of tea a day is not enough to take advantage of the full benefits that it offers. It is believed that 2-5  cups of green tea a day offers the maximum benefit. If you do not want to make green tea several times a day, consider trying organic green tea supplements."
+   
+          },  
+      {
+          id: 8,
+       image: "images/ice-cold cofee.jpg",
+       name: "Cold-Coffee With Icecream ",
+       category: "Beverage",
+       price: "70₹",
+       description:
+         "In the recipe vanilla ice cream is blended with the coffee and milk, which gives a thicker, creamier cold coffee. The coffee is then topped with scoops of vanilla ice cream and served. I have used homemade vanilla ice cream, but you can even use chocolate ice cream or coffee ice cream."
+   
+          },  
+      {
+          id: 8,
+          image: "images/ice creame.jpg",
+          name: "Icecream ",
+          category: "Beverage",
+          price: "60₹",
+          description:
+            "Ice cream (derived from earlier iced cream or cream ice) is a sweetened frozen food typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavored with a sweetener, either sugar or an alternative, and any spice, such as cocoa or vanilla"
+      },
+
+      {
+        id: 9,
+        image: "images/juice.jpg",
+        name: "Juice",
+        category: "Beverage",
+        price: "80₹",
+        description:
+        "Juice is a drink made from the extraction or pressing of the natural liquid contained in fruit and vegetables. It can also refer to liquids that are flavored with concentrate or other biological food sources, such as meat or seafood, such as clam juice."
+    }
+];
+export default Menu;
